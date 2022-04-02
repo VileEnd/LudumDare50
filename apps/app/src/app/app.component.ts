bc83260ef577d070f8d35e9924ca95b0f1fdf240
jan kretschmer <jk.code@protonmail.com>
2022-04-02 18:49:14 +0200

    inital commit

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import * as PIXI from 'pixi.js'
-import { fromEvent } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { htmlaudio, sound } from '@pixi/sound'
+
+//Step:1
+//ToDo: Implement a interactable object
+// ToDo: implement custom background
+
+// Step2
+// ToDo: Animate Object
+// ToDo: implement Sound
 
 export abstract class Actor extends PIXI.Graphics {
+
 }
 
 
 class Background extends Actor {
+
 }
 
 @Component({
@@ -15,10 +24,12 @@ class Background extends Actor {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   private app: PIXI.Application = new PIXI.Application({
     width: window.innerWidth,
     height: window.innerHeight,
+    antialias:true,
   });
 
   private actor: Actor[]=[];
@@ -26,5 +37,10 @@ export class AppComponent {
   ngOnInit(): void {
     document.body.appendChild(this.app.view);
     this.actor.push(new Background());
+    // const bg =  PIXI.Texture.fromLoader("../assets/",'../assets/')
+    const texture = PIXI.Texture.from('../assets/testdawg.png');
+    const sprite1 = new PIXI.Sprite(texture);
+    sprite1.anchor.set(0.5)
+
   }
 }
