f7180896988b75ca5fd803e58d401f3c61c4fcc8
jan kretschmer <jk.code@protonmail.com>
2022-04-02 17:37:46 +0200

    change to Angular

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js'
+import { fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export abstract class Actor extends PIXI.Graphics {
+}
+
+
+class Background extends Actor {
+}
 
 @Component({
   selector: 'catakiller-root',
@@ -6,5 +16,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  private app: PIXI.Application = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  private actor: Actor[]=[];
+
+  ngOnInit(): void {
+    document.body.appendChild(this.app.view);
+    this.actor.push(new Background());
+  }
 }
